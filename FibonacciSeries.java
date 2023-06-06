31b7e851d75f1ac63f48a573e5c0255564906cf4
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-07 00:07:27 +0800

    Program 3 completed.

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -4,11 +4,17 @@ public class FibonacciSeries {
     public static void main(String[] args) {
         int n = 10;
         int t1 = 0, t2 = 1;
-        
+
         System.out.print("First " + n + " terms: ");
 
-        /*
-        insert your code here
-        */
+        for (int i = 0; i <n; i++) {
+            System.out.print(t1 + " ");
+
+            int sum = t1 + t2; // Calculate the next term
+            t1 = t2; // Update t1 with the value of t2
+            t2 = sum; // Update t2 with the sum
+
+            // The loop will repeat until all the terms are generated
+        }
     }
 }
