658fdf813cc34fc4318a78b8dd09eaf8827af9c1
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-06 23:52:19 +0800

    Initial commit for Program 3: Fibonacci Series.

---
+++ FibonacciSeries.java
@@ -0,0 +1,14 @@
+//Fibonacci Series: (0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55)
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 10;
+        int t1 = 0, t2 = 1;
+        
+        System.out.print("First " + n + " terms: ");
+
+        /*
+        insert your code here
+        */
+    }
+}
