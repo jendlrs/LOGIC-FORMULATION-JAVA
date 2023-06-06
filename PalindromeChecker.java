4118746e69fe795aec3a3ed28d77e36ab934bee1
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-07 00:45:50 +0800

    Initial commit for Program 5: Palindrome Check.

---
+++ PalindromeChecker.java
@@ -0,0 +1,10 @@
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        String original = "racecar";
+        String reversed = "";
+
+        /*
+        Insert code solution here
+        */
+    }
+}
