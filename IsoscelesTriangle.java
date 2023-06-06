d326c8c89b287243eaded66446a1e6a765560c25
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-07 00:16:27 +0800

    Initial Commit for program 4: Printing Isosceles Triangle using asterisks. I just added steps as a guide.

---
+++ IsoscelesTriangle.java
@@ -0,0 +1,12 @@
+public class IsoscelesTriangle {
+    public static void main(String[] args) {
+        int rows = 5; // Number of rows in the triangle
+        
+        //Loop
+            //Inside loop to print spaces
+            
+            //Loop to print asterisks
+
+        //Move to the next line
+    }
+}
