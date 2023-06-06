dd7735373c2bee35600f2a93d7c309d0b4b15ebb
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-06 23:47:14 +0800

    Program 2 completed.

--- PrimeNumChecker.java
+++ PrimeNumChecker.java
@@ -4,9 +4,15 @@ public class PrimeNumChecker {
     public static void main(String[] args) {
         int num = 29; // this is the number to test
         boolean isPrime = true;
-    /*
-    Insert your code here
-    */
+    
+        //Check if the number is divisible by any integer up to the square root of num
+        for (int i = 2; i<=Math.sqrt(num); i++){
+            if (num % i == 0){
+                isPrime = false; //if divisible, num is not prime
+                break; 
+            }
+        }
+
         if(isPrime) {
             System.out.println(num + " is a prime number.");
         } else {
