27556286fcf37773d63e7b84ad54e76d597dd80c
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-06 23:23:55 +0800

    Initial Commit for Program 2: Checking for Prime Number

---
+++ PrimeNumChecker.java
@@ -0,0 +1,16 @@
+/*Checking for a Prime Number */
+
+public class PrimeNumChecker {
+    public static void main(String[] args) {
+        int num = 29; // this is the number to test
+        boolean isPrime = true;
+    /*
+    Insert your code here
+    */
+        if(isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
