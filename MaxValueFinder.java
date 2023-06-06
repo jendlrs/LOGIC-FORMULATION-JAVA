711fc5c03acec6562f735199974c91c84c0d7f6f
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-06 23:03:14 +0800

    Initial Commit for Program 1: Finding the Maximum Value in an Array.

---
+++ MaxValueFinder.java
@@ -0,0 +1,16 @@
+/* Finding the Maximum Value in an Array */
+
+public class Main {
+    public static void main(String[] args) {
+
+    int[] numbers = {1, 3, 5, 7, 9, 2, 4, 6, 8};
+    int max = numbers[0];
+
+/*
+Process here the max value will be save to max variable
+use loop
+*/
+
+    System.out.println("The maximum value in the array is: " + max);
+    }
+}
