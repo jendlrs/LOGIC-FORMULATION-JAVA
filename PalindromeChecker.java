7e9b7e20e237dec5317475abff61d5119175da76
Ma. Jensen Nicole Dela Rosa <jensennics.dlrs@gmail.com>
2023-06-07 00:59:27 +0800

    Program 5 completed. Tested other word, and phrases, it is working.

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -3,8 +3,17 @@ public class PalindromeChecker {
         String original = "racecar";
         String reversed = "";
 
-        /*
-        Insert code solution here
-        */
+        //Reverse the original string
+        for (int i = original.length()-1; i >= 0; i--){
+            reversed += original.charAt(i);
+        }
+        
+        //Check if the original and reversed strings are equal.
+        if (original.equals(reversed)){
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not palindrome.");
+        }
+        
     }
 }
